483eea56b0852c7fc7e52d629587b71ed2ad4e5b
STS677-PAVITHRA V <STS677-PAVITHRA V@STS677L-PAVITHR>
2022-10-13 12:28:48 +0530

    Add project files.

---
+++ WebApplication3/Controllers/custom.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class custom : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Custome()
+        {
+            return View();
+        }
+        public IActionResult Customer3(customer2 data)
+        {
+            using (var entity = new CustomerContext()) 
+            {
+
+
+                Table_1 cus = new Table_1();
+             
+                cus.Customer_Name = data.CustomerName;
+                cus.Customer_Address = data.CustomerAddress;
+
+                entity.Table_1.Add(cus);
+                entity.SaveChanges();
+
+            }
+            return View();
+        }
+
+    }
+}
---
+++ WebApplication3/Models/customer2.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication3.Models
+{
+    public class customer2
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+    }
+}
